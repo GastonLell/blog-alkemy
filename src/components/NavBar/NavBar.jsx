97552892947e6c4849a4dd954cf1bfcab390ca97
GastonLell <gastonglell@gmail.com>
2021-05-20 01:55:07 -0300

    creating the navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,42 @@
+//REACT ROUTER
+import { Link, useHistory } from "react-router-dom";
+import {useEffect, useState} from 'react';
+//COMPONENTER BOOTSTRAP
+import { Navbar, Button, Col } from "react-bootstrap";
+
+//REACT ICONS
+import { FaSignOutAlt } from "react-icons/fa";
+
+
+const NavBar = () => {
+
+  const history = useHistory();
+
+  const handleSignOut = () => {
+
+    localStorage.removeItem('token');
+
+    history.push('/login')
+
+  }
+
+  return (
+    <Navbar bg="dark" variant="dark" className="px-4 py-3">
+
+      <Col className="px-0 text-left">
+        <Navbar.Brand>
+          <Link 
+            style={{"text-decoration": "none", "color": "#fff"}}
+            to='/'
+          >AlkePosts</Link>
+        </Navbar.Brand>
+      </Col>
+      
+      <Col className="d-flex justify-content-end" >
+        <Button variant="outline-info" onClick={handleSignOut}><FaSignOutAlt/></Button>
+      </Col>
+
+    </Navbar>
+  );
+};
+export default NavBar;
--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
+// COMPONENTS REACT BOOTSTRAP
 import { BrowserRouter, Switch, Route} from "react-router-dom";
-import Home from './pages/Home';
-import Login from './pages/Login';
+// COMPONENTS
+
+import NavBar from "./components/NavBar/NavBar";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 const Router = () => {
     return(
         <BrowserRouter>
+            <NavBar/>
             <Switch>
                 <PrivateRoute exact path="/" component={Home} />
                 <Route exact path="/login" component={Login} />
